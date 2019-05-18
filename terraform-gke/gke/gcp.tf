37db3bd6a369d528f04eb728a1454fe32d4db569
Tailp <33935517+Tailp@users.noreply.github.com>
2019-05-18 21:41:45 +0200

    Update gcp.tf

--- terraform-gke/gke/gcp.tf
+++ terraform-gke/gke/gcp.tf
@@ -2,7 +2,7 @@
 # Google Cloud Platform
 #####################################################################
 provider "google" {
-	credentials = "${file("/home/tailp/jenkinscrt/natural-metric-235613-eeea69237ccf.json")}"
+	credentials = "${file("/home/tailp/Works/Devops/Demo/natural-metric-235613-a44060dc139e.json")}"
 	project = "${var.project}"
 	region  = "${var.region}"
 }
